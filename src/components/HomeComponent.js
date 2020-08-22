bd167d03afa06e1ce0d5f0ce5049b4c69a4db770
Sreenivas <rsreenivas2001@gmail.com>
2020-08-22 19:20:34 +0530

    rm baseUrl

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle} from 'reactstrap';
-import { baseUrl } from '../shared/baseUrl';
+
 
 function RenderCard({item}) {
 
     return(
         <Card>
-            <CardImg src={ baseUrl + item.image} alt={item.name} />
+            <CardImg src={  item.image} alt={item.name} />
             <CardBody>
             <CardTitle>{item.name}</CardTitle>
             {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
